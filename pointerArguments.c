48f07776d521bb76b387256f90bfad49a6f645f0
‘Anahita <anahita.safari@gmail.com>
2018-11-15 13:23:49 -0600

    adding some comments to the code.

--- pointerArguments.c
+++ pointerArguments.c
@@ -1,5 +1,10 @@
 
+////Anahita Safari
 #include<stdio.h>
+
+// this function has two return values and uses pointer arguments two return two different values.
+// This function gets an argent arg and returns multiplication by 2 and 3 of that argument.
+
 
 int func1(int arg, int *ret1, int *ret2){
 	* ret1= 2 * arg;
